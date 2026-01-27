1060e91f65ad1c11efdb5fdd63162a0bdf3d61f6
jialiang.sun <jialiang.sun@rwth-aachen.de>
2026-01-27 16:04:09 +0100

    only modify syst in histogrammer.py

--- src/BTVNanoCommissioning/utils/histogramming/histogrammer.py
+++ src/BTVNanoCommissioning/utils/histogramming/histogrammer.py
@@ -1,4 +1,5 @@
 import awkward as ak
+import numpy as np
 from BTVNanoCommissioning.helpers.func import flatten
 from .hist_helpers import get_hist_collections, get_axes_collections
 
@@ -141,6 +142,7 @@ def histo_writter(pruned_ev, output, weights, systematics, isSyst, SF_map):
             if syst == "nominal" or syst not in list(weights.variations)
             else weights.weight(modifier=syst)
         )
+        syst = np.full(len(weight), syst)
         # Loop over the histograms
         for histname, h in output.items():
             # Tagger score histograms
